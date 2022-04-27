e2b9faa22bc43addc44e7784adeff7c519804d14
Nitya Agarwal <n3agarwa@ucsd.edu>
2022-04-27 12:28:22 -0700

    Inserted PlanActivity + PlanButton

--- app/src/main/java/edu/ucsd/cse110/zooseeker46/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/MainActivity.java
@@ -2,7 +2,9 @@ package edu.ucsd.cse110.zooseeker46;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlanButtonClicked(View view) {
+        Intent intent = new Intent(this, PlanActivity.class);
+        startActivity(intent);
     }
 }
