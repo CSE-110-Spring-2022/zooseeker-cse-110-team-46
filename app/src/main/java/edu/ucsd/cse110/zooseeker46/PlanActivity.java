c69dd9faabad89bcc29e913832e26efcb538902f
Nitya Agarwal <n3agarwa@ucsd.edu>
2022-04-27 12:52:01 -0700

    Added showAlert for unimplemented buttons

--- app/src/main/java/edu/ucsd/cse110/zooseeker46/PlanActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/PlanActivity.java
@@ -3,6 +3,7 @@ package edu.ucsd.cse110.zooseeker46;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class PlanActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class PlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+    }
+
+    public void onDirectionsButtonClicked(View view) {
+        Utilities.showAlert(this, "This button doesn't do anything yet!");
     }
 }
