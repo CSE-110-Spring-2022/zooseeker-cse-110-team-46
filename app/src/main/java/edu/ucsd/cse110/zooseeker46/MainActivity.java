4efc821ce27fd1379364b44a9f11a65fe88ec666
BOT KEV <53915339+ckckcake@users.noreply.github.com>
2022-05-01 15:18:05 -0700

    Update MainActivity.java

--- app/src/main/java/edu/ucsd/cse110/zooseeker46/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/MainActivity.java
@@ -17,30 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(this, SearchActivity.class);
-/*
-//start of exhibit count updater
-        //getting planned/selected exhibit count
-        //this probably needs to be changed to get exhibit count from ZooExhibits.class
-        TextView exhibitCountView = findViewById(R.id.exhibit_count);
-        String exhibitCountStr = exhibitCountView.getText().toString();
-
-        //Check if integer parsed  correctly
-        Optional<Integer> maybeExhibitCount = Utilities.parseCount(exhibitCountStr);
-        if (!maybeExhibitCount.isPresent()) {
-            //if not, show an error and return
-            Utilities.showAlert(this, "Didn't parse count correctly");
-            return;
-        }
-
-        //get the integer
-        int exhibitCount = maybeExhibitCount.get();
-
-        //intent
-        intent.putExtra("exhibit_count", exhibitCount);
-
-//end of exhibit count updater
-
-*/
         startActivity(intent);
     }
 
