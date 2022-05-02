c8be4b3ce5d59d845b62acdd9d54bbdce4415c77
Harini Gurusankar <harinigurusankar@Harinis-MacBook-Air.local>
2022-05-01 21:53:04 -0700

    ZooExhibits REWRITE

--- app/src/main/java/edu/ucsd/cse110/zooseeker46/ZooExhibits.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/ZooExhibits.java
@@ -1,11 +1,37 @@
 package edu.ucsd.cse110.zooseeker46;
 
-public class ZooExhibits {
-    int exhibit_count;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ZooExhibits extends AppCompatActivity {
+    Map<String, Exhibit> exhibitMap = Collections.emptyMap();
+    List<String> idList = new ArrayList<>();
+    ArrayList<Exhibit> exhibitArrayList = new ArrayList<>();
+    ArrayList<String> selectedExhibits = new ArrayList<>();
 
     public ZooExhibits(){
-        exhibit_count = 0;
+        Map<String, ZooData.VertexInfo> vertexInfoMap = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
+        for(Map.Entry<String, ZooData.VertexInfo> entry : vertexInfoMap.entrySet()){
+            ZooData.VertexInfo item = entry.getValue();
+            String name = entry.getValue().name;
+            Exhibit exhibit = new Exhibit(item.id, item.name, item.tags);
+            exhibitMap.put(name, exhibit);
+            exhibitArrayList.add(exhibit);
+        }
     }
-//test
+
+    public void getID(String name) {
+        for (Map.Entry<String, Exhibit> entry: exhibitMap.entrySet()){
+            Exhibit exhibit = entry.getValue();
+            if ((exhibit.getName().equals(name)) && (exhibit.isSelected)){
+                idList.add(exhibit.getId());
+                selectedExhibits.add(exhibit.getName());
+            }
+        }
+    }
 }
 
