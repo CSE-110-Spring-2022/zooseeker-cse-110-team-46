7abbd481fcfa80501e12bed85376e3b6fcc2b7e1
Nitya Agarwal <n3agarwa@ucsd.edu>
2022-05-11 12:01:37 -0700

    Added file misc.xml to .gitignore and Warning for Database fixed

--- app/src/main/java/edu/ucsd/cse110/zooseeker46/database/ZooDataDatabase.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/database/ZooDataDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.TypeConverters;
 import edu.ucsd.cse110.zooseeker46.locations.Exhibit;
 import edu.ucsd.cse110.zooseeker46.locations.languageConverterTags;
 
-@Database(entities = {Exhibit.class}, version = 1)
+@Database(entities = {Exhibit.class}, version = 1, exportSchema = false)
 @TypeConverters({languageConverterTags.class})
 public abstract class ZooDataDatabase extends RoomDatabase {
     public abstract ExhibitDao exhibitDao();
