159834496879ac20669e1547ec241ab01cde866e
Kevin Lam <klam2965@gmail.com>
2022-05-26 16:00:36 -0700

    Dylan helps

--- app/src/main/java/edu/ucsd/cse110/zooseeker46/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import edu.ucsd.cse110.zooseeker46.database.ZooDataDatabase;
 import edu.ucsd.cse110.zooseeker46.search.SearchActivity;
 
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Uncomment for debugging:
+        ZooDataDatabase.setShouldForceRepopulate();
 
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
--- app/src/main/java/edu/ucsd/cse110/zooseeker46/database/ZooDataDatabase.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker46/database/ZooDataDatabase.java
@@ -14,6 +14,7 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -33,12 +34,23 @@ public abstract class ZooDataDatabase extends RoomDatabase {
     private static ZooDataDatabase singleton = null;
 
     static ZooData zooDataObj = new ZooData();
+
+    public static void setShouldForceRepopulate() {
+        ZooDataDatabase.shouldForceRepopulate = true;
+    }
+
+    private static boolean shouldForceRepopulate = false;
+
     public abstract ExhibitDao exhibitDao();
+
     public abstract GateDao gateDao();
+
     public abstract IntersectionDao intersectionDao();
+
     public abstract ExhibitGroupDao exhibitGroupDao();
 
 
+    //creating/getting singleton
     public synchronized static ZooDataDatabase getSingleton(Context context) {
         if (singleton == null) {
             String msg = "Singleton is null!";
@@ -48,6 +60,7 @@ public abstract class ZooDataDatabase extends RoomDatabase {
         return singleton;
     }
 
+    //making db from "sample_node_info.json" and adding to db with the appropriate DAO depending on "kind"
     private static ZooDataDatabase makeDatabase(Context context) {
         return Room.databaseBuilder(context, ZooDataDatabase.class, "ZooData.db")
                 .allowMainThreadQueries()
@@ -55,48 +68,64 @@ public abstract class ZooDataDatabase extends RoomDatabase {
                     @Override
                     public void onCreate(@NonNull SupportSQLiteDatabase db) {
                         super.onCreate(db);
-                        ZooDataDatabase zb = ZooDataDatabase.getSingleton(context);
-                        //Single thread for singleton
                         Executors.newSingleThreadExecutor().execute(() -> {
-                            Map<String, ZooData.VertexInfo> info =
-                                    zooDataObj.loadVertexInfoJSON(context,"sample_node_info.json");
-                            Log.d("size of map --- : ", String.valueOf(info.size()));
-                            //checking format and calling constructor accordingly to add object
-                            for (Map.Entry<String,ZooData.VertexInfo> entry : info.entrySet()) {
-                                ZooData.VertexInfo curr = entry.getValue();
-                                //if (entry.getValue().kind.equals("exhibit")) {
-                                if (entry.getValue().kind == EXHIBIT) {
-                                    String pid = "";
-                                    if(!(curr.parent_id.equals(""))){
-                                        pid = curr.parent_id;
-                                    }
-                                    Exhibit exhibitObj = new Exhibit(curr.id, curr.name, pid, curr.tags,0 , 0);
-                                    //getSingleton(context).exhibitDao().insert(exhibitObj);
-                                    zb.exhibitDao().insert(exhibitObj);
-                                } else if (entry.getValue().kind.equals("gate")) {
-                                    Gate gateObj = new Gate(curr.id, curr.name, curr.tags,0 , 0);
-                                    //getSingleton(context).gateDao().insert(gateObj);
-                                    zb.gateDao().insert(gateObj);
-
-                                } else if (entry.getValue().kind.equals("intersection")) {
-                                    Intersection intersectionObj = new Intersection(curr.id, curr.name, curr.tags,0 , 0);
-                                    //getSingleton(context).intersectionDao().insert(intersectionObj);
-                                    zb.intersectionDao().insert(intersectionObj);
-                                } else {
-                                    Exhibit_Group exhibit_groupObj = new Exhibit_Group(curr.id, curr.name, 0 ,0);
-                                    //getSingleton(context).exhibitGroupDao().insert(exhibit_groupObj);
-                                    zb.exhibitGroupDao().insert(exhibit_groupObj);
-                                }
-                            }
+                            ZooDataDatabase zb = ZooDataDatabase.getSingleton(context);
+                            Map<String, ZooData.VertexInfo> info = ZooData.loadVertexInfoJSON(context, "sample_node_info.json");
+                            populateDatabase(zb, info);
                         });
+                    }
+
+                    @Override
+                    public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                        super.onOpen(db);
+                        if (shouldForceRepopulate) {
+                            Executors.newSingleThreadExecutor().execute(() -> {
+                                ZooDataDatabase zb = ZooDataDatabase.getSingleton(context);
+                                zb.clearAllTables();
+                                Map<String, ZooData.VertexInfo> info = ZooData.loadVertexInfoJSON(context, "sample_node_info.json");
+                                populateDatabase(zb, info);
+                            });
+                        }
                     }
                 })
                 .build();
     }
 
+    private static void populateDatabase(ZooDataDatabase zb, Map<String, ZooData.VertexInfo> info) {
+        //Single thread for singleton
+        Log.d("size of map --- : ", String.valueOf(info.size()));
+        //checking format and calling constructor accordingly to add object
+        for (Map.Entry<String, ZooData.VertexInfo> entry : info.entrySet()) {
+            ZooData.VertexInfo curr = entry.getValue();
+            //if (entry.getValue().kind.equals("exhibit")) {
+
+            Kind kind = entry.getValue().kind;
+            if (kind == Kind.EXHIBIT) {
+                Exhibit exhibitObj = new Exhibit(curr.id, curr.name, curr.parent_id, curr.tags, 0, 0);
+                //getSingleton(context).exhibitDao().insert(exhibitObj);
+                zb.exhibitDao().insert(exhibitObj);
+            } else if (kind == Kind.GATE) {
+                Gate gateObj = new Gate(curr.id, curr.name, curr.tags, 0, 0);
+                //getSingleton(context).gateDao().insert(gateObj);
+                zb.gateDao().insert(gateObj);
+            } else if (kind == Kind.INTERSECTION) {
+                Intersection intersectionObj = new Intersection(curr.id, curr.name, curr.tags, 0, 0);
+                //getSingleton(context).intersectionDao().insert(intersectionObj);
+                zb.intersectionDao().insert(intersectionObj);
+            } else if (kind == Kind.EXHIBIT_GROUP) {
+                Exhibit_Group exhibit_groupObj = new Exhibit_Group(curr.id, curr.name, 0, 0);
+                //getSingleton(context).exhibitGroupDao().insert(exhibit_groupObj);
+                zb.exhibitGroupDao().insert(exhibit_groupObj);
+            } else {
+                // "unreachable" error.
+                throw new RuntimeException("Unknown kind! What have you done!?");
+            }
+        }
+    }
+
     @VisibleForTesting
-    public static void injectTestDatabase(ZooDataDatabase testDatabase){
-        if(singleton != null) {
+    public static void injectTestDatabase(ZooDataDatabase testDatabase) {
+        if (singleton != null) {
             singleton.close();
         }
         singleton = testDatabase;
